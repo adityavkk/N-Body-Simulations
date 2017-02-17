af7bfa2c8d951442eff6401bb78c94643add792d
Aditya K <adityavkk@gmail.com>
2017-02-16 23:21:53 -0500

    get the moon in line! -- adjust init velocity

--- Direct-Simulation/src/SolarSystem.hs
+++ Direct-Simulation/src/SolarSystem.hs
@@ -11,13 +11,13 @@ earthD   = 152098232.0e3
 earthV   =  29.78e3
 masses =
   map (* earthM)
-    [0.0553,	0.815, 1, 0.0123,	0.107,	317.8,	95.2,	14.5,	17.1,	0.0025]
+    [0.0553, 0.815, 1, 0.0123, 0.107,	317.8, 95.2, 14.5, 17.1, 0.0025]
 distances =
   map (* earthD)
-    [0.387,	0.723, 1,	1.00257, 1.52, 5.20,	9.58,	19.20, 30.05,	39.48]
+    [0.387,	0.723, 1,	1.00257, 1.52, 5.20, 9.58,	19.20, 30.05,	39.48]
 initVelocities =
   map (* earthV)
-    [1.59,	1.18,	1, 	0.0343,	0.808,	0.439,	0.325,	0.228,	0.182,	0.157]
+    [1.59, 1.18, 1, 1.0343, 0.808, 0.439, 0.325, 0.228,	0.182, 0.157]
 colors = [G.greyN 0.5, G.violet, G.chartreuse, G.greyN 0.2, G.red, G.orange, G.chartreuse, G.azure, G.cyan, G.magenta]
 
 planets = zipWith4 f masses distances initVelocities colors
