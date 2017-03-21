06a150e36e2949ccff2ad6984e12b1287d0d9d02
Aditya K <adityavkk@gmail.com>
2017-03-21 12:08:51 -0400

    start on a universal pdf function with a numeric integration technique

--- Barnes-Hut/src/RandomBodies.hs
+++ Barnes-Hut/src/RandomBodies.hs
@@ -6,17 +6,27 @@ import Utils
 import Numeric.Tools.Integration
 import Numeric.Tools.Differentiation
 
-plummerRho :: Mass -> Radius -> Pos -> Float
-plummerRho m a p = ((3 * m) / (4 * pi)) * (a^2 / (a^2 + r^2)**(5/2))
-  where r = d (P 0 0) p
+plummerRho :: Mass -> Radius -> Float -> Float
+plummerRho m a r = ((3 * m) / (4 * pi)) * (a^2 / (a^2 + r^2) ** (5/2))
 
-hernquistRho :: Mass -> Radius -> Pos -> Float
-hernquistRho m a p = (m / (2 * pi)) * (a / (r * (a + r)^3))
-  where r = d (P 0 0) p
+hernquistRho :: Mass -> Radius -> Float -> Float
+hernquistRho m a r = (m / (2 * pi)) * (a / (r * (a + r)^3))
 
 plummerPDF :: Mass -> Radius -> Pos -> Float
 plummerPDF m a p = m * (r^3 / (r^2 + a^2) ** (3/2))
   where r = d (P 0 0) p
+
+pdf ::
+  Radius
+  -> (Mass -> Radius -> Pos -> Float)
+  -> Mass
+  -> Radius
+  -> Pos
+  -> Float
+  -> Float
+pdf d rho m r p = quadBestEst $ quadRomberg defQuad (0, d) rho'
+  where rho' :: Double -> Double
+        rho' x = rho m r 
 
 vonNeumannSampling ::
   Int
